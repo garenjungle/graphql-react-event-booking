bdbbea19e6f5a4c183e81f59dfd055004a33087d
garenjungle <kykps2@gmail.com>
2019-01-10 00:40:09 +0900

    youtube #12 complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import { AuthPage, BookingsPage, EventsPage } from './pages';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -9,12 +10,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect exact from="/" to="/auth" />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect exact from="/" to="/auth" />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './MainNavigation.css';
+
+const mainNavigation = props => {
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation__logo">
+        <h1>EasyEvent</h1>
+      </div>
+      <nav className="main-navigation__items">
+        <ul>
+          <li>
+            <NavLink to="/Auth">Authenticate</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Bookings">Bookings</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default mainNavigation;
